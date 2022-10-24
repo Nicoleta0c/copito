700e12f53a512bd47cb807c7d65600a15c932af1
Nicolle Rosa Andujar <niicostars04@gmail.com>
2022-10-24 16:57:42 -0400

    ...

--- maps.js
+++ maps.js
@@ -1,71 +1,95 @@
 const emojis={
-'-':' ',
-'O':'🚪​​​​',
-'X':'💣',
-'I':'🍨​​',
-'PLAYER':'☃️​​',
-'BOMB_COLLISION':'🔥',
-'GAME_OVER':'👎',
-'WIN':'🏆',
-'HEART': '❤️',
-};
-
-const maps=[];
-maps.push(`
-  IXXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  OXXXXXXXXX
-`);maps.push(`
-  O--XXXXXXX
-  X--XXXXXXX
-  XX----XXXX
-  X--XX-XXXX
-  X-XXX--XXX
-  X-XXXX-XXX
-  XX--XX--XX
-  XX--XXX-XX
-  XXXX---IXX
-  XXXXXXXXXX
+  '-':' ',
+  'O':'🚪​​​​',
+  'X':'💣',
+  'I':'🍨​​',
+  'PLAYER':'🐧​',
+  'BOMB_COLLISION':'🔥',
+  'GAME_OVER':'👎',
+  'WIN':'🏆',
+  'HEART': '❤️',
+  };
+  
+  const maps=[];
+  maps.push(`
+    IXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    OXXXXXXXXX
   `);maps.push(`
-  I-----XXXX
-  XXXXX-XXXX
-  XX----XXXX
-  XX-XXXXXXX
-  XX-----XXX
-  XXXXXX-XXX
-  XX-----XXX
-  XX-XXXXXXX
-  XX-----OXX
-  XXXXXXXXXX
-`);
-/*maps.push(`
-  XXXXXXXXXI
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX-
-  XXXXXXXXX0
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+    `);maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
   `);
-*/ maps.push(`
-OXXXXXXXXX
--XXXXXXX-X
--X-IXXXXXX
--XX-XXXX-X
--X-------X
--XX-XX-XXX
--XX-XX-XXX
--XXX-X-XXX
--XX-XX-XXX
--------XXX
-`);
+  maps.push(`
+  OXXXXXXXXX
+  -XX----X-X
+  -X-IXX-XXX
+  -XXXXX-X-X
+  -----X---X
+  -XX-XX-XXX
+  -XX-XX-XXX
+  -XXXXX-XXX
+  -XXXXX-XXX
+  -------XXX
+  `);
+  maps.push(`
+  XXX------X
+  XXX-XXXX-X
+  XXXOXXXX-X
+  XXXXXX---X
+  IXXXXX-XXX
+  --X----XXX
+  X-X-XXXXXX
+  X-X----XXX
+  X-XXXX-XXX
+  X------XXX
+  `);
+  maps.push(`
+  XXXXXXXXXX
+  ---------X
+  IXXXXXXX-X
+  XXXX---X-X
+  O--X-X-X-X
+  XX-X-X-X-X
+  XX-X-X-X-X
+  XX-X-X-X-X
+  XX---X---X
+  XXXXXXXXXX
+  `);
+  maps.push(`
+  XXXX-----X
+  XXXX-XXXIX
+  O-XX---XXX
+  X-XXXX-XXX
+  X--XXX--XX
+  XX---XX-XX
+  XXXX-XX-XX
+  X----XX-XX
+  X-XXXXX-XX
+  X--------X
+  `);
